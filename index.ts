76330baf6a4362881818d3fd5d72ddb5b8e4e68d
farooq1986siddiqui <125255480+farooq1986siddiqui@users.noreply.github.com>
2023-04-09 19:55:35 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,42 @@
+#! /usr/bin/env node
+
+import { add } from "./add.js";
+import inquirer from "inquirer"
+
+const asnwers = await inquirer.prompt([
+    {
+        name: "num1",
+        message: "Enter first number",
+        type: "number"
+    },
+    {
+        name: "num2",
+        message: "Enter second number",
+        type: "number"
+    }
+])
+
+
+const result = add(asnwers.num1, asnwers.num2)
+console.log(result)
+
+
+
+
+
+
+
+// Program
+// Program
+// Program
+// Program
+// Program
+// Program
+// Program
+// Program
+
+
+// const ans = add(3, 5);
+// console.log(ans);
+
+// console.log("hello")
